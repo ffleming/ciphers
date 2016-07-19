ed8662174c9227fae57093f2b82fcbd9fb3d5055
Forrest Fleming <ffleming@gmail.com>
2016-07-18 19:25:06 -0700

    Autokey examples from wikipedia

--- spec/autokey_spec.rb
+++ spec/autokey_spec.rb
@@ -3,4 +3,12 @@ describe Ciphers::Autokey do
   it_should_behave_like "a cipher",
     name: "Practical Cryptography", plaintext: "DEFENDTHEEASTWALLOFTHECASTLE",
     ciphertext: "ISWXVIBJEXIGGZEQPBIMOIGAKMHE", key: "FORTIFICATION"
+
+  it_should_behave_like "a cipher",
+    name: "Wikipedia 1", plaintext: "MEETATTHEFOUNTAIN",
+    ciphertext: "WMPMMXXAEYHBRYOCA", key: "KILT"
+
+  it_should_behave_like "a cipher",
+    name: "Wikipedia 2", plaintext: "ATTACKATDAWN",
+    ciphertext: "QNXEPVYTWTWP", key: "QUEENLY"
 end
